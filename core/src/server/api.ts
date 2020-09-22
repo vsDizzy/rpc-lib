25c6a8590301aa43fb060cfa263a454ce1dd6dab
Pavel <todizzy@gmail.com>
2020-09-22 04:48:25 +0300

    Simplify Api signature to handle ts2536 when compiled

--- core/src/server/api.ts
+++ core/src/server/api.ts
@@ -1,6 +1,6 @@
-export type Api<T> = MapProps<Pick<T, FilterProps<T>[keyof T]>>;
-
-type MapProps<T> = { [P in keyof T]: ReplaceReturnType<T[P]> };
+export type Api<T> = {
+  [P in keyof T]: ReplaceReturnType<T[P]>;
+};
 
 type ReplaceReturnType<T> = T extends IsFunction<T>
   ? ReturnType<T> extends Promise<unknown>
@@ -9,7 +9,3 @@ type ReplaceReturnType<T> = T extends IsFunction<T>
   : never;
 
 type IsFunction<T> = T extends (...args: unknown[]) => unknown ? T : never;
-
-type FilterProps<T> = {
-  [P in keyof T]: T[P] extends IsFunction<T[P]> ? P : never;
-};
